0acb3564fff2fc002ce714980a92f2f33c95eed4
likeri29 <s.pochtaruk29@gmail.com>
2023-10-20 22:57:00 +0200

    finished charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -21,7 +21,7 @@ const Repos = () => {
 
     return total;
   }, {});
-  console.log(languages);
+
   const mostUsed = Object.values(languages)
     .sort((a, b) => b.value - a.value)
     .slice(0, 5);
@@ -33,13 +33,26 @@ const Repos = () => {
     })
     .slice(0, 5);
 
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    { stars: {}, forks: {} }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <div></div>
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <div></div>
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
