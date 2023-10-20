db267ae29792ddf74ca9479080685a33a5ec63b5
likeri29 <s.pochtaruk29@gmail.com>
2023-10-20 17:02:44 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
